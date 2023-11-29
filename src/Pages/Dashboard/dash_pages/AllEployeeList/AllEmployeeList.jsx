08d20463773baeedba574a3703f4babb6008c750
Jahid Kham <jahidkhan12xx@gmail.com>
2023-11-29 12:24:21 +0600

    admin and hr fix

--- src/Pages/Dashboard/dash_pages/AllEployeeList/AllEmployeeList.jsx
+++ src/Pages/Dashboard/dash_pages/AllEployeeList/AllEmployeeList.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import SectionHeader from "../../../../components/shared/SectionHeader";
 import useUserData from "../../../../hooks/useUserData";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import toast from "react-hot-toast";
+import { FaTable } from "react-icons/fa";
+import { CiCreditCard2 } from "react-icons/ci";
+
 
 
 const AllEmployeeList = () => {
+    const [view,setView] = useState(true);
     const axiosSecure = useAxiosSecure();
     const [userData,isLoading,refetch] = useUserData();
     const [users,setUsers] = useState();
@@ -43,44 +47,55 @@ const AllEmployeeList = () => {
     }
 
     console.log(users);
+    
+    
     return (
         <div>
             <SectionHeader header={"Employee List"}></SectionHeader>
-            <div className="overflow-x-auto">
-  <table className="table table-zebra">
-    {/* head */}
-    <thead className=" text-xl font-bold">
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Designation</th>
-        <th>Make HR</th>
-        <th>Fire</th>
-      </tr>
-    </thead>
-    <tbody className=" text-xl font-light ">
-      {
-        users?.map((us,idx) =>  <tr key={us._id}>
-            <td>{idx+1}</td>
-            <td>{us.name}</td>
-            <td className=" uppercase ">{us.role}</td>
-            <td className="">
+           <div className=" ml-5">
+           <h2 className=" text-lg">{view?"Table view" : "Card view"}</h2>
+            <button onClick={()=> setView(!view)} className=" btn btn-primary">{view? <FaTable/>:<CiCreditCard2/>}</button>
+           </div>
+           {
+            view ?  <div className="overflow-x-auto">
+            <table className="table table-zebra">
+              {/* head */}
+              <thead className=" text-xl font-bold">
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Designation</th>
+                  <th>Make HR</th>
+                  <th>Fire</th>
+                </tr>
+              </thead>
+              <tbody className=" text-xl font-light ">
                 {
-                    us.role == "hr" ? <h2 className=" ml-5">HR</h2> :<button onClick={()=>handleHr(us.email,us.name)} className=" btn bg-blue-700 rounded-none text-white btn-sm btn-ghost">Make HR</button>
+                  users?.map((us,idx) =>  <tr key={us._id}>
+                      <td>{idx+1}</td>
+                      <td>{us.name}</td>
+                      <td className=" uppercase ">{us.role}</td>
+                      <td className="">
+                          {
+                              us.role == "hr" ? <h2 className=" ml-5">HR</h2> :<button onClick={()=>handleHr(us.email,us.name)} className=" btn bg-blue-700 rounded-none text-white btn-sm btn-ghost">Make HR</button>
+                          }
+                          
+                          </td>
+                      <td>
+                          {
+                              us.isFired ? <h2 className=" text-red-800 font-semibold">Fired</h2> : <button onClick={()=>handleFire(us.email,us.name)} className=" btn btn-sm btn-ghost">X</button>
+                          }
+                      </td>
+                    </tr>)
                 }
-                
-                </td>
-            <td>
-                {
-                    us.isFired ? <h2 className=" text-red-800 font-semibold">Fired</h2> : <button onClick={()=>handleFire(us.email,us.name)} className=" btn btn-sm btn-ghost">X</button>
-                }
-            </td>
-          </tr>)
-      }
-     
-      </tbody>
-      </table>
-      </div>
+               
+                </tbody>
+                </table>
+                </div> : 
+                <div className="">
+                     Card
+                </div>
+           }
         </div>
     );
 };
